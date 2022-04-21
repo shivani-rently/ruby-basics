0749108f0d744c348a3eefe7e4f98e645d31d617
Shivani <shivani.r@rently.com>
2022-04-21 17:11:39 +0530

    authentication completed

--- app/app.rb
+++ app/app.rb
@@ -1,20 +1,52 @@
 require_relative "utils.rb"
 
-def form
-    puts "Username: "
+def signup
+    print "Username: "
     name = gets.chomp
     db = open_db
     if db.has_key?(name)
-        print "User Exists"
+        puts "User Exists"
     else  
-        puts "Password: "
+        print "Email Id: "
+        email = gets.chomp
+        print "Password: "
         pswd =gets.chomp
-        db[name] = {"name":name, "pswd":pswd}
+        db[name] = {"name":name,"email":email,"pswd":pswd}
         write_to_db(db)
-        print "User Created"
+        puts "User Created"
+    end
+end
+
+def signin
+    print "Username: "
+    name = gets.chomp
+    db = open_db
+    if db.has_key?(name)
+        print "Password: "
+        pswd =gets.chomp
+        if db[name]["pswd"] == pswd
+            puts "User Logged In"
+        else
+            puts "Incorrect Password"
+        end
+    else
+        puts "User doesn't exist"
+    end
+end
+
+def menu
+    puts "Welcome\n1. Signin\n2. Signup"
+    print "Option: "
+    option = gets.chomp.to_i
+    if option == 1
+        signin
+    elsif option == 2
+        signup
+    else
+        puts "Option Unavailable"
     end
 end
 
 if __FILE__ == $0
-    form
+    menu
 end
